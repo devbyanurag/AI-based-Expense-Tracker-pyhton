0175a4ed62276ac244fef009467bb042247a7097
devbyanurag <karanchheti@gmail.com>
2024-10-31 20:20:32 +0530

    Added torch and tranformers

--- main.py
+++ main.py
@@ -9,3 +9,8 @@ app.include_router(health.router, prefix="/health", tags=["Health"])
 @app.get("/")
 async def startup():
     return "Api is working fine v1"
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
