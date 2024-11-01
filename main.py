eca18456aa5b158227fef9e427610c1b0b3e5632
devbyanurag <karanchheti@gmail.com>
2024-11-01 12:19:01 +0530

    test llm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from app.routes import health
-
+import os
 app = FastAPI()
 
 # Include routers from separate route modules
@@ -10,3 +10,7 @@ app.include_router(health.router, prefix="/health", tags=["Health"])
 async def startup():
     return "Api is working fine v1"
 
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
